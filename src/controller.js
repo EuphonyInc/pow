0d41100ecff0103599dbdce7d5fc3e260f870682
Peter B Smith <peterbutlersmith@gmail.com>
2016-08-23 11:33:19 -0400

    preparing pow for alpha release
    making the code fully functional for the happy case, splitting out
    files, and sucessfully testing manually

---
+++ src/controller.js
@@ -0,0 +1,20 @@
+app.controller('AudioCtrl',function($scope, voiceFactory) {
+  $scope.data = {};
+
+  $scope.data.text = "Hello";
+  $scope.data.voice = "en_US";
+  $scope.data.audioType = "WAVE_FILE";
+  $scope.data.audio = {};
+  $scope.update = function() {
+    console.log('okay')
+    $scope.data.audio = {
+      text: $scope.data.text,
+      voice: $scope.data.voice,
+      audioType: $scope.data.audioType
+    };
+
+    voiceFactory.getAudio($scope.data.audio).then(function(arrayBuffer) {
+      $scope.data.arrayBuffer= arrayBuffer;
+    });
+  };
+});
